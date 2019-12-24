3fcbabe04940b05875ad98db69b56f7400f64c62
Alpha <abc_cn@163.com>
2019-12-24 17:32:40 +0800

    invocation handler

--- src/main/java/proxy/TankD.java
+++ src/main/java/proxy/TankD.java
@@ -27,9 +27,9 @@ public class TankD implements Movable {
 
 
         // reflection - 通过字节码分析类的属性和方法
-        Movable m = (Movable) Proxy.newProxyInstance(TankD.class.getClassLoader(),
-                new Class[]{Movable.class},
-                new InvocationHandler(){
+        Movable m = (Movable) Proxy.newProxyInstance(TankD.class.getClassLoader(), // ClassLoader() 类的加载器
+                new Class[]{Movable.class},     // 实现的接口数组
+                new InvocationHandler(){        // 接口内部类实现
 
                     @Override
                     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
